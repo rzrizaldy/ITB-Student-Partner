b2cfa068e4869c6e843903706cee2fc9c9b2af9e
18214046 - Fat Han Nuraddin <18214046@std.stei.itb.ac.id>
2016-12-18 13:54:33 +0700

    Update koneksi.php

--- 14002/koneksi.php
+++ 14002/koneksi.php
@@ -4,6 +4,12 @@
 	$username = "sql6148414";
 	$password = "JVZsZGZDqF";
 	$dbname = "sql6148414";
+	
+	//UPDATE
+	$servername = "sql6.freemysqlhosting.net";
+	$username = "sql6149851";
+	$password = "eVFKBMIua7";
+	$dbname = "sql6149851";
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
